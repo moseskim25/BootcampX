fbf09091139129adb9477477c565527200f60688
Moses Kim <moseskim4740@gmail.com>
2021-05-26 20:46:20 +0000

    updated using parameterized query

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,6 +1,4 @@
 const { Pool } = require('pg');
-const cohort_month = process.argv[2];
-const num_results = process.argv[3];
 
 const pool = new Pool({
   user: 'vagrant',
@@ -9,15 +7,24 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+
+
+const cohort_month = process.argv[2];
+const limit = process.argv[3] || 5;
+values = [`%${cohort_month}%`, limit];
+
+const queryString = `
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
   })
-}).catch(err => console.error('query error', err.stack));
+}).catch(err => console.error('query error', err.stack))
+.finally(pool.end());
